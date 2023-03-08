5a78c196aaf8037fc9adcfda6128f9c64dee3618
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-03-08 14:04:16 -0500

    fixing syntax errors

--- script.js
+++ script.js
@@ -52,22 +52,19 @@ $(document).ready(function () {
 
 
 
+  $("#hour-8 .description").val(localStorage.getItem("hour8"));
+  $("#hour-9 .description").val(localStorage.getItem("hour9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour17"));
 
 
 
-  $("hour8 .description").val(localStorage.getItem("hour8"));
-  $("hour9 .description").val(localStorage.getItem("hour9"));
-  $("hour10 .description").val(localStorage.getItem("hour10"));
-  $("hour11 .description").val(localStorage.getItem("hour11"));
-  $("hour12 .description").val(localStorage.getItem("hour12"));
-  $("hour13 .description").val(localStorage.getItem("hour13"));
-  $("hour14 .description").val(localStorage.getItem("hour14"));
-  $("hour15 .description").val(localStorage.getItem("hour15"));
-  $("hour16 .description").val(localStorage.getItem("hour16"));
-  $("#hour17 .description").val(localStorage.getItem("hour17"));
-
-
-  
 
   timeTracker();
 

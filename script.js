17f0d2acd4ecf95398b17fd67f853112a7786e01
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-03-08 13:56:47 -0500

    commiting changes to test

--- script.js
+++ script.js
@@ -52,31 +52,28 @@ $(document).ready(function () {
 
 
 
-  // $("#clearFieldsButton").on("click", function () {
-  //   ("#clearFieldsBtn").click(function (event) {
-  //     event.preventDefault;
-  //     $("textArea").val("");
-  //     localStorage.clear();
-
-
-      $("hour8 .description").val(localStorage.getItem("hour8"));
-      $("hour9 .description").val(localStorage.getItem("hour9"));
-      $("hour10 .description").val(localStorage.getItem("hour10"));
-      $("hour11 .description").val(localStorage.getItem("hour11"));
-      $("hour12 .description").val(localStorage.getItem("hour12"));
-      $("hour13 .description").val(localStorage.getItem("hour13"));
-      $("hour14 .description").val(localStorage.getItem("hour14"));
-      $("hour15 .description").val(localStorage.getItem("hour15"));
-      $("hour16 .description").val(localStorage.getItem("hour16"));
-      $("#hour17 .description").val(localStorage.getItem("hour17"));
-
-
-      timeTracker();
 
 
 
-    });
+  $("hour8 .description").val(localStorage.getItem("hour8"));
+  $("hour9 .description").val(localStorage.getItem("hour9"));
+  $("hour10 .description").val(localStorage.getItem("hour10"));
+  $("hour11 .description").val(localStorage.getItem("hour11"));
+  $("hour12 .description").val(localStorage.getItem("hour12"));
+  $("hour13 .description").val(localStorage.getItem("hour13"));
+  $("hour14 .description").val(localStorage.getItem("hour14"));
+  $("hour15 .description").val(localStorage.getItem("hour15"));
+  $("hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-  // });
 
   
+
+  timeTracker();
+
+
+
+});
+
+
+
